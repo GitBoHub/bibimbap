983ea73875276d0eb426447d45bd11269bda47fc
Rey Cegar Pier Castaneda Haro <rey@bespoken.tools>
2017-08-29 18:19:26 -0500

    finish first part, frizz forecast

--- index.js
+++ index.js
@@ -4,46 +4,45 @@
 var request = require('request');
 
 
-const frizzFactor = {
-    reallyLow: {
-        defaultResponse: "the same",
-        response: [
-            "Straight hair don’t care.",
-            "Hot dog! Go on, warm up that hair straightener.",
-            "And a high chance of static electricity.",
-            "Whip your straight hair back and forth.",
-            "One does not simply straighten their hair every day. Only on days like today.",
-            "I don’t always have straight hair, but when I do I check with Frizz Forecast."
-        ],
-        level: [15]
-    },
-    low: {
-        defaultResponse: "the same",
+const frizzFactorArray = [{
+    defaultResponse: "Dry, with a chance of straight hair",
+    response: [
+        "Straight hair don’t care.",
+        "Hot dog! Go on, warm up that hair straightener.",
+        "And a high chance of static electricity.",
+        "Whip your straight hair back and forth.",
+        "One does not simply straighten their hair every day. Only on days like today.",
+        "I don’t always have straight hair, but when I do I check with Frizz Forecast."
+    ],
+    level: 15,
+},
+    {
+        defaultResponse: "Low, with a small chance of frizzle",
         response: [
             "Hey girl, your hair is hardly frizzy at all today.",
             "Challenge accepted, frizz forecast.",
             "Don’t cry over frizzy hair.",
             "Don't worry. Frizz is temporary."
         ],
-        level: [15, 30]
+        level: 30,
     },
-    medium: {
-        defaultResponse: "the same",
+    {
+        defaultResponse: "Partly frizzy",
         response: [
-            "With enough hairspray, I think you’ll avoid a frizz-aster.", 
-            "To curl or not to curl. That is the question.", 
-            "The hair tie: Never leave home without it.", 
-            "Ermahgerd, what’s your hair going to do today?", 
-            "First world frizz problems.", 
-            "Make waves. Literally, make hair waves.", 
+            "With enough hairspray, I think you’ll avoid a frizz-aster.",
+            "To curl or not to curl. That is the question.",
+            "The hair tie: Never leave home without it.",
+            "Ermahgerd, what’s your hair going to do today?",
+            "First world frizz problems.",
+            "Make waves. Literally, make hair waves.",
             "Challenge accepted, mother nature."
         ],
-        level: [30, 40]
+        level: 40,
     },
-    mediumHigh: {
-        defaultResponse: "the same",
+    {
+        defaultResponse: "Clear for curls",
         response: [
-            "Go on, let your hair down. Today is perfect for curls. Yassss."
+            "Go on, let your hair down. Today is perfect for curls. Yassss.",
             "Who run the world? Curls. Go ahead, get your curl on today.",
             "Awwwww yeah. Curly hair. Don’t care.",
             "Life is short. Let your curls out today.",
@@ -52,10 +51,10 @@ const frizzFactor = {
             "Big hair don’t care. I’m Amazon Alexa and I support this message.",
             "Footloose and frizz free."
         ],
-        level: [40, 60]
+        level: 60,
     },
-    high: {
-        defaultResponse: "the same",
+    {
+        defaultResponse: "Severe frizz warning",
         response: [
             "Sorry, I can’t hear you over the volume of your hair.",
             "Is your hair big today because it’s full of secrets?",
@@ -67,11 +66,12 @@ const frizzFactor = {
             "Let it fro. Let it fro. Let it fro.",
             "All’s fair in love and frizz.",
             "This frizz shall pass."
-          ],
-          level: [60]
-     };
+        ],
+        level: 61,
+    }
+];
 
-exports.handler = function(event, context) {
+exports.handler = function (event, context) {
     var player = new FrizzForecast(event, context);
     player.handle();
 };
@@ -91,7 +91,13 @@ FrizzForecast.prototype.handle = function () {
     } else if (requestType === "IntentRequest") {
         var intent = this.event.request.intent;
         if (intent.name === "frizzForecast") {
-            this.getWeatherForecast(this.event.request);
+            const consentToken = this.event.context ?
+                this.event.context.System.user.permissions && this.event.context.System.user.permissions.consentToken :
+                this.event.session.user.permissions && this.event.session.user.permissions.consentToken;
+            const deviceId = this.event.context ?
+                this.event.context.System.device && this.event.context.System.device.deviceId :
+                "";
+            this.getWeatherForecast(deviceId, consentToken);
         } else if (intent.name === "AMAZON.PauseIntent") {
 
         } else if (intent.name === "AMAZON.ResumeIntent") {
@@ -133,26 +139,48 @@ FrizzForecast.prototype.say = function (message, repromptMessage) {
 
 /**
  * Gets the frizz forecast ased on the current weather
- * @param request
+ * @param deviceId
+ * @param consentToken
  */
-FrizzForecast.prototype.getWeatherForecast = function (request) {
-    // call weather API get info for frizzFactor keys.level > 54  < 54
-    // proccess code dewpoint
-    // frizzFactorRandom = frizzFactor.mediumhigh.random
-    console.log(request);
-    // request('http://api.wunderground.com/api/4a9c079f1cdb5b80/geolookup/conditions/q/IA/Cedar_Rapids.json', function (error, response, body) {
-    //     console.log('statusCode:', response['location']['city']);
-    // });
-
-    var response = {
-        version: "1.0",
-        response: {
-            shouldEndSession: true,
-            outputSpeech: {
-                type: "SSML",
-                ssml: "<speak> Your frizz forecast for today is frizz.defaultResponse, FrizzFactorRandom</speak>"
-            }
+FrizzForecast.prototype.getWeatherForecast = function (deviceId, consentToken) {
+    const currentContext = this.context;
+    request.get('https://api.amazonalexa.com/v1/devices/' + deviceId + '/settings/address/countryAndPostalCode', {
+        'auth': {
+            'bearer': consentToken
         }
-    };
-    this.context.succeed(response);
+    }, function (error, response, body) {
+        const country = body.countryCode || "CA";
+        const zipCode = body.postalCode || "94016";
+        const url = 'http://api.wunderground.com/api/4a9c079f1cdb5b80/conditions/q/' + country + '/' + zipCode + '.json';
+        let userFactor;
+        request(url, function (wuError, wuResponse, wuBody) {
+            const parsed = JSON.parse(wuBody);
+            const dewpoint = parsed.current_observation && parsed.current_observation.dewpoint_f;
+            for (i = 0; frizzFactorArray.length; i++) {
+                if (frizzFactorArray[i].level > dewpoint) {
+                    userFactor = frizzFactorArray[i];
+                    break;
+                } else if (i === 4) {
+                    userFactor = frizzFactorArray[i];
+                }
+            }
+            const finalMessage = "Your frizz forecast for today is " + userFactor.defaultResponse + ", " + getRandomQuote(userFactor) + " the Temperature";
+            console.log(finalMessage);
+            var response = {
+                version: "1.0",
+                response: {
+                    shouldEndSession: true,
+                    outputSpeech: {
+                        type: "SSML",
+                        ssml: "<speak>" + finalMessage + "</speak>"
+                    }
+                }
+            };
+            currentContext.succeed(response);
+        });
+    });
 };
+
+function getRandomQuote(userFactor) {
+    return userFactor.response[Math.floor(Math.random() * userFactor.response.length)];
+}
