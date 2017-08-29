05f32fa81ccfcea29207d383f617cdab23a37581
GitBoHub <bo@bespoken.tools>
2017-08-29 15:15:18 -0700

    Frizz factor update

--- index.js
+++ index.js
@@ -9,16 +9,67 @@ const frizzFactor = {
         defaultResponse: "the same",
         response: [
             "Straight hair don’t care.",
-            "",
-            ""
+            "Hot dog! Go on, warm up that hair straightener.",
+            "And a high chance of static electricity.",
+            "Whip your straight hair back and forth.",
+            "One does not simply straighten their hair every day. Only on days like today.",
+            "I don’t always have straight hair, but when I do I check with Frizz Forecast."
         ],
         level: [15]
     },
-    low: [],
-    medium: [],
-    mediumHigh: { response: ["Go on, let your hair down. Today is perfect for curls. Yassss.", "", ""], level: [40, 60]},
-    high: [],
-};
+    low: {
+        defaultResponse: "the same",
+        response: [
+            "Hey girl, your hair is hardly frizzy at all today.",
+            "Challenge accepted, frizz forecast.",
+            "Don’t cry over frizzy hair.",
+            "Don't worry. Frizz is temporary."
+        ],
+        level: [15, 30]
+    },
+    medium: {
+        defaultResponse: "the same",
+        response: [
+            "With enough hairspray, I think you’ll avoid a frizz-aster.", 
+            "To curl or not to curl. That is the question.", 
+            "The hair tie: Never leave home without it.", 
+            "Ermahgerd, what’s your hair going to do today?", 
+            "First world frizz problems.", 
+            "Make waves. Literally, make hair waves.", 
+            "Challenge accepted, mother nature."
+        ],
+        level: [30, 40]
+    },
+    mediumHigh: {
+        defaultResponse: "the same",
+        response: [
+            "Go on, let your hair down. Today is perfect for curls. Yassss."
+            "Who run the world? Curls. Go ahead, get your curl on today.",
+            "Awwwww yeah. Curly hair. Don’t care.",
+            "Life is short. Let your curls out today.",
+            "Love is in the hair. Today brings prime curls conditions.",
+            "Curlies have more fun. Now, go out there and prove it.",
+            "Big hair don’t care. I’m Amazon Alexa and I support this message.",
+            "Footloose and frizz free."
+        ],
+        level: [40, 60]
+    },
+    high: {
+        defaultResponse: "the same",
+        response: [
+            "Sorry, I can’t hear you over the volume of your hair.",
+            "Is your hair big today because it’s full of secrets?",
+            "Can I call you Miss Frizzle?",
+            "There's a 100 percent chance of a ponytail.",
+            "I see a ponytail in your future.",
+            "Take the necessary precautions, and go for a classic bun.",
+            "How are you with updos?",
+            "Let it fro. Let it fro. Let it fro.",
+            "All’s fair in love and frizz.",
+            "This frizz shall pass."
+          ],
+          level: [60]
+     };
 
 exports.handler = function(event, context) {
     var player = new FrizzForecast(event, context);
